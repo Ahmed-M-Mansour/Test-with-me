e76d6f28c2ce6207d9468bea302f44f4f45d4ac5
Ahmed-M-Mansour <ahmedmmansour18@gmail.com>
2022-02-21 00:31:36 +0200

    finish basic  validation

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
-document.getElementById('button').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "flex";
+// Handle The Popup Window 
+document.getElementById("button").addEventListener("click", function () {
+  document.querySelector(".bg-modal").style.display = "flex";
+});
+// Close The Popup Window 
+document.querySelector(".close").addEventListener("click", function () {
+  document.querySelector(".bg-modal").style.display = "none";
 });
 
-document.querySelector('.close').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "none";
-});
+/*
+Form Validation 
+Get the inputs values from the Document object  
+*/
+const firstName = document.getElementById("first__name");
+const lastName = document.getElementById("last__name");
+const email = document.getElementById("email");
+const number = document.getElementById("number");
 
- 
+const formErorr = document.getElementById('error')
+const fprmCompleted = document.getElementById('completed')
+
+console.log(formErorr)
+console.log(fprmCompleted)
+
+
+
+// Handle Form Submition 
+  addEventListener("submit", (e) => {
+	  e.preventDefault() ; 
+	  // Get the value of the inputs to check it 
+	const firstNameValue = firstName.value ; 
+	const lastNameValue = lastName.value ; 
+	const emailValue = email.value ; 
+	const numberValue = number.value ; 
+
+	// Validators Inputs 
+    const validatorFirsName = validator.isAlpha(firstNameValue); 
+    const validatorLastName = validator.isAlpha(lastNameValue); 
+    const validatorEmail = validator.isEmail(emailValue); 
+    const validatorPhoneNumber = validator.isMobilePhone(numberValue); 
+	// check if the inputs are Correct 
+	if(validatorFirsName && validatorLastName && validatorEmail && validatorPhoneNumber ) {
+		formErorr.style["display"] = "none"
+		fprmCompleted.style["display"] = "block"
+	}
+	// check if the inputs are NOT Correct
+	else {
+		fprmCompleted.style["display"] = "none"
+		formErorr.style["display"] = "block"
+	}
+  });
+
